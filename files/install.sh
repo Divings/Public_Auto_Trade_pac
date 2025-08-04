1fd64c02e8d560f85b9bcdcd1a89a5855ec15606
Divings <144668090+Divings@users.noreply.github.com>
2025-08-05 07:41:13 +0900

    インストールスクリプトを修正

--- files/install.sh
+++ files/install.sh
@@ -1,8 +1,23 @@
 #!/usr/bin/bash
 
+set -e  # エラーがあったらスクリプトを即終了
+
+# リポジトリを追加
 cp fx.repo /etc/yum.repos.d/
+
+# キャッシュを更新
 yum makecache
-yum install fx_autotrade-system -y
+
+# RPMパッケージと依存パッケージをインストール
+yum install -y python3-pip fx_autotrade-system
+
+# systemd サービスの登録
 cp fx-autotrade.service /etc/systemd/system/
 systemctl daemon-reload
+
+# Python依存パッケージをインストール
+python3 -m pip install --upgrade pip
+python3 -m pip install -r requirements.txt
+
+# セットアップスクリプトの実行
 python3 /opt/Innovations/System/Setup.py
