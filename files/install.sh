99b5b142f4201404717db405f83cc30aac9ae3c6
Divings <144668090+Divings@users.noreply.github.com>
2026-01-08 11:36:40 +0900

    インストールスクリプトを修正

--- files/install.sh
+++ files/install.sh
@@ -11,6 +11,9 @@ yum makecache
 # RPMパッケージと依存パッケージをインストール
 yum install -y python3-pip fx_autotrade-system
 
+# GPGのインストール
+yum install -y gpg 
+
 # systemd サービスの登録
 cp fx-autotrade.service /etc/systemd/system/
 systemctl daemon-reload
@@ -18,6 +21,11 @@ systemctl daemon-reload
 # Python依存パッケージをインストール
 python3 -m pip install --upgrade pip
 python3 -m pip install -r requirements.txt
+
+# スクリプトを所定のディレクトリにコピー
+mkdir -p /opt/tools/
+cp get_logs.py /opt/tools/ # ログ取得スクリプトのコピー
+cp Start.sh /opt/tools # 起動スクリプトのコピー
 
 # セットアップスクリプトの実行
 python3 /opt/Innovations/System/Setup.py
