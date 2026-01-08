57ebb98c6efd043f01fb1115b6d28718e9f41304
Divings <144668090+Divings@users.noreply.github.com>
2026-01-08 13:07:35 +0900

    インストールスクリプトを最適化

--- files/install.sh
+++ files/install.sh
@@ -1,6 +1,13 @@
-#!/usr/bin/bash
+#!/usr/bin/env bash
 
 set -e  # エラーがあったらスクリプトを即終了
+cd "$(dirname "$0")"
+
+# rootユーザーでの実行を確認
+if [ "$EUID" -ne 0 ]; then
+  echo "This script must be run as root"
+  exit 1
+fi
 
 # リポジトリを追加
 cp fx.repo /etc/yum.repos.d/
@@ -17,6 +24,7 @@ yum install -y gpg
 # systemd サービスの登録
 cp fx-autotrade.service /etc/systemd/system/
 systemctl daemon-reload
+systemctl enable fx-autotrade.service
 
 # Python依存パッケージをインストール
 python3 -m pip install --upgrade pip
@@ -26,6 +34,10 @@ python3 -m pip install -r requirements.txt
 mkdir -p /opt/tools/
 cp get_logs.py /opt/tools/ # ログ取得スクリプトのコピー
 cp Start.sh /opt/tools # 起動スクリプトのコピー
+chmod +x /opt/tools/Start.sh
 
+# ディレクトリ移動
+cd /opt/Innovations/System/
 # セットアップスクリプトの実行
 python3 /opt/Innovations/System/Setup.py
+
